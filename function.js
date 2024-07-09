e0d53d43341c04f13b31c219a5690bc69c1e7224
new-college <omkarpatil98986029@gmail.com>
2024-07-09 17:43:39 +0530

    practice

--- function.js
+++ function.js
@@ -41,3 +41,40 @@
 // }
 // let result = vow("Javascript");
 // console.log(result);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+// let name = "Linda"
+// let greeting = "Hi there"
+
+// // Create a function that logs out "Hi there, Linda!" when called
+
+// function greetLinda() {
+//     console.log(greeting + ", " + name + "!")
+// }
+
+// greetLinda()
+
+
+// let myPoints = 3
+
+// // Create two functions, add3Points() and remove1Point(), and have them
+// // add/remove points to/from the myPoints variable
+
+// function add3Points() {
+//     myPoints += 3
+// }
+
+// function remove1Points() {
+//     myPoints -= 1
+// }
+
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Points()
+// remove1Points()
+
+// // Call the functions to that the line below logs out 10
+// console.log(myPoints)
